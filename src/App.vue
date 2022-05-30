2ba62e62645728739366e22034f3c8c2f278ddf9
sudhanshulicensed <sudhanshu.licensed@gmail.com>
2022-05-30 11:36:16 +0530

    Added a text with routed link

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </nav> -->
+    <router-link to="/about">About</router-link>
     <router-view/>
   </div>
 </template>
